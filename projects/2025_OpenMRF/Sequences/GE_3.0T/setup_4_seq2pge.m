d7f9d07de50fe3b31956580f545eca48485b4223
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2025-12-18 17:15:25 -0500

    Turn off Pulseq warning for arbitrary gradients On branch main 	modified:   setup_4_seq2pge.m

--- projects/2025_OpenMRF/Sequences/GE_3.0T/setup_4_seq2pge.m
+++ projects/2025_OpenMRF/Sequences/GE_3.0T/setup_4_seq2pge.m
@@ -1,6 +1,7 @@
 % Get Pulseq toolbox
 system('git clone --branch v1.5.1 git@github.com:pulseq/pulseq.git');
 addpath pulseq/matlab
+warning('OFF', 'mr:restoreShape');  % turn off Pulseq warning for spirals
 
 % Get toolbox to convert .seq file to a .pge file for execution on GE
 system('git clone --branch main git@github.com:HarmonizedMRI/PulCeq.git');
