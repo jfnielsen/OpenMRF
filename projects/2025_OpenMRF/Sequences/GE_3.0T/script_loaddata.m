d4147432f106d3525f886ca424adf8d9c0645da6
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2026-02-12 18:48:59 -0500

    On branch main 	modified:   script_loaddata.m

--- projects/2025_OpenMRF/Sequences/GE_3.0T/script_loaddata.m
+++ projects/2025_OpenMRF/Sequences/GE_3.0T/script_loaddata.m
@@ -1,20 +1,5 @@
-p = pge2.utils.normalizepath('/home/jon/transfer/Exam8521');
-
-% pge724.entry, 251116_0308_mgram_mrf_yun
-d = pge2.utils.loaddata([p 'Series4/ScanArchive_7347633TMRFIX_20260212_162040809.h5']);
-save -v7.3 d724.mat d
-
-return
-
-% pge722.entry, 251116_0307_mgram_wasabi
-d = pge2.utils.loaddata([p 'Series3/ScanArchive_7347633TMRFIX_20260212_154428315.h5']);
-save -v7.3 d722.mat d
-
-return
 
 p = pge2.utils.normalizepath('/home/jon/dropbox/Shared/Pulseq/OpenMRF/20260212/Exam8521');
-
-if 0
 
 % pge722.entry, wasabi
 S = [3 7 8 13];  % series numbers
@@ -23,9 +8,12 @@ for s = S
     F = strsplit(ls(pth, '-tr'));   % .h5 file names
     for ii = 1:length(F)-1
         d = pge2.utils.loaddata([pth F{ii}]);
+        save(['d_wasabi_series' num2str(s) '_run' num2str(ii)]);
+        pause(2);
     end
-    save(['d_wasabi_series' num2str(s) '_run' num2str(ii)]);
 end
+
+return
 
 % pge724.entry, mrf_yun
 S = [4 9];  % series numbers
@@ -34,9 +22,9 @@ for s = S
     F = strsplit(ls(pth, '-tr'));   % .h5 file names
     for ii = 1:length(F)-1
         d = pge2.utils.loaddata([pth F{ii}]);
+        save(['d_mrf_yun_series' num2str(s) '_run' num2str(ii)]);
+        pause(2);
     end
-    save(['d_mrf_yun_series' num2str(s) '_run' num2str(ii)]);
-end
 end
 
 % pge726.entry, cmrf_t1_t2
@@ -46,8 +34,9 @@ for s = S
     F = strsplit(ls(pth, '-tr'));   % .h5 file names
     for ii = 1:length(F)-1
         d = pge2.utils.loaddata([pth F{ii}]);
+        save(['d_cmrf_t1_t2_series' num2str(s) '_run' num2str(ii)]);
+        pause(2);
     end
-    save(['d_cmrf_t1_t2_series' num2str(s) '_run' num2str(ii)]);
 end
 
 % pge728.entry, cmrf_t1_t2_t1p_300Hz
@@ -57,23 +46,8 @@ for s = S
     F = strsplit(ls(pth, '-tr'));   % .h5 file names
     for ii = 1:length(F)-1
         d = pge2.utils.loaddata([pth F{ii}]);
+        save(['d_cmrf_t1_t2_g1p_300Hz_series' num2str(s) '_run' num2str(ii)]);
+        pause(2);
     end
-    save(['d_cmrf_t1_t2_g1p_300Hz_series' num2str(s) '_run' num2str(ii)]);
 end
-
-return
-
-d = pge2.utils.loaddata([p 'ScanArchive_7347633TMRFIX_20260212_154428315.h5']);
-save -v7.3 d722.mat d
-
-return
-
-d = pge2.utils.loaddata([p 'Series4/ScanArchive_7347633TMRFIX_20251218_182031072.h5']);
-save -v7.3 d724.mat d
-
-d = pge2.utils.loaddata([p 'Series5/ScanArchive_7347633TMRFIX_20251218_182136207.h5']);
-save -v7.3 d726.mat d
-
-d = pge2.utils.loaddata([p 'Series6/ScanArchive_7347633TMRFIX_20251218_182526493.h5']);
-save -v7.3 d728.mat d
 
