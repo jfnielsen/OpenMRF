fd2c779d115441ac7157348807889e857308805a
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2026-04-09 20:44:38 -0400

    On branch main 	modified:   script_loaddata.m

--- projects/2025_OpenMRF/Sequences/GE_3.0T/script_loaddata.m
+++ projects/2025_OpenMRF/Sequences/GE_3.0T/script_loaddata.m
@@ -7,15 +7,18 @@ p = normalizepath('/home/jon/transfer/Exam8797');
 S = [11 12 13 14 15];
 opuser1 = [721 722 724 725 726];
 
-% load 5 most recent raw data files and save each to .mat file
+% load 5 most recent raw data files and save each to .mat file, except for wasabi
 assert(numel(S) == numel(opuser1));
 for ii = 1:length(S)
     pth = normalizepath([p 'Series' num2str(S(ii))]);
     F = strsplit(ls(pth, '-tr'));   % .h5 file names
-    for jj = 1 %:5
+    for jj = 1:5
         d = pge2.utils.loaddata([pth F{end-jj}]);
         %fprintf('%s\n', [pth F{end-jj}]);
         save(sprintf('d_Series_%d_opuser1_%d_run_%d', S(ii), opuser1(ii), jj), 'd', '-v7.3');
+        if opuser1(ii) == 721
+            break;   % wasabi
+        end
     end
     pause(1);
 end
